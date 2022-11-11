7ebf8469fecbbd678db0f6ab45d3b83ff12f55f0
AnkitM2212 <107674699+AnkitM2212@users.noreply.github.com>
2022-11-11 15:28:13 +0530

    Update docker-steps.sh

--- docker-steps.sh
+++ docker-steps.sh
@@ -1,5 +1,6 @@
 # Login in docker
-docker login -u "${DOCKER_HUB_USER}" -p "${DOCKER_HUB_PASSWORD}"
+#docker login -u "${DOCKER_HUB_USER}" -p "${DOCKER_HUB_PASSWORD}"
+docker "$DOCKER_HUB_PASSWORD" | docker login -u "$DOCKER_HUB_USER" --password-stdin
 
 # build docker image
 docker build -t hello-world-python .
